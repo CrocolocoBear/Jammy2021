747b0e9a16c4cc6b724a2887bdd92cd02a9ae669
AaronJeffers1996 <47363247+AaronJeffers1996@users.noreply.github.com>
2021-01-30 00:25:14 +0000

    prototype push

--- Assets/Prototype/Programmers/Aaron's Folder/PlayerPrototype.cs
+++ Assets/Prototype/Programmers/Aaron's Folder/PlayerPrototype.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class PlayerPrototype : MonoBehaviour
 {
     Rigidbody rb;
+    public GameObject ring;
     public List<Transform> arms;
     private float velcoity;
     float rotation = 0;
     Vector3 normalVelocityX;
     Vector3 normalVelocityZ;
     Camera cam;
+    bool throwing = false;
+    Vector3 ringOGPos;
+    float throwingSpeed = 50;
 
     private void Awake()
     {
         rb = this.GetComponent<Rigidbody>();
         cam = this.GetComponentInChildren<Camera>();
+        ringOGPos = ring.transform.position - cam.transform.position;
     }
 
     private void Start()
@@ -27,40 +32,40 @@ public class PlayerPrototype : MonoBehaviour
     {
         PlayerInput();
         Camera();
+        if(throwing) 
+        {
+           Throw();
+        }
+        Debug.Log(ringOGPos);
     }
 
 
     private void PlayerInput()
     {
-        Vector3 desiredVelocity = new Vector3(0,0,0);
+        Vector3 desiredVelocity = new Vector3(0, 0, 0);
         float speed = 0;
 
-        //if(Input.GetButton("Vertical"))
-        //{
-        //    desiredVelocity.z = 5 * Input.GetAxis("Vertical");
-        //}
-        //if (Input.GetButton("Horizontal"))
-        //{
-        //    desiredVelocity.x = 5 * Input.GetAxis("Horizontal");
-        //}
         if (Input.GetButton("Vertical") || Input.GetButton("Horizontal"))
         {
-            for(int i = 0; i < arms.Count; i++)
+            for (int i = 0; i < arms.Count; i++)
             {
-                arms[i].Rotate(200 * Time.deltaTime,0,0);
+                arms[i].Rotate(200 * Time.deltaTime, 0, 0);
             }
             normalVelocityX = Input.GetAxis("Horizontal") * transform.right;
             normalVelocityZ = Input.GetAxis("Vertical") * transform.forward;
             speed = 5;
         }
-        if(Input.GetAxis("Mouse X") != 0)
+        if (Input.GetAxis("Mouse X") != 0)
         {
             rotation += 2 * Input.GetAxis("Mouse X");
         }
-        
+        if(Input.GetMouseButtonDown(0))
+        {           
+            throwing = true;
+        }
+
         desiredVelocity = (normalVelocityX + normalVelocityZ) * speed;
         Move(desiredVelocity, rotation);
-        Debug.Log(desiredVelocity);
     }
 
     private void Move(Vector3 vel, float r)
@@ -68,7 +73,24 @@ public class PlayerPrototype : MonoBehaviour
         rb.velocity = vel;
         rb.rotation = Quaternion.Euler(0, r, 0);
     }
-    
+
+    private void Throw()
+    {
+        
+        ring.transform.localPosition += new Vector3(0,0, throwingSpeed) * Time.deltaTime;
+        if (Vector3.Distance(ring.transform.position, cam.transform.position) > 20)
+        {
+            throwingSpeed *= -1;           
+        }
+
+        if (Vector3.Distance(ring.transform.position, cam.transform.position + (cam.transform.forward * ringOGPos.z) + (cam.transform.up * ringOGPos.y)) <= 0.2)
+        {
+            ring.transform.position = cam.transform.position + (cam.transform.forward * ringOGPos.z) + (cam.transform.up * ringOGPos.y);
+            throwingSpeed *= -1;
+            throwing = false;
+        }
+    }
+
     private void Camera()
     {
         float rotSpeed = -2f;
